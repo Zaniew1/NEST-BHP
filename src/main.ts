7a096ce4595826eff5d20a114b8af3cbe47887aa
Mateusz <m.zaniewski1995@gmail.com>
2025-04-02 20:00:32 +0200

    chore: use .env variables in main.ts and set GlobalPrefixes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { PORT, NODE_ENV, APP_VERSION } from './helpers/env';
+import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  // app.setGlobalPrefix(`api/${APP_VERSION}`)
+  // app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     whitelist: true
+  //   })
+  // )
+  await app.listen(PORT);
 }
 bootstrap();
